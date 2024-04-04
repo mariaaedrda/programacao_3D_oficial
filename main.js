ac9d3abe3ecca0e1e5328d42c87ded0a710383e8
mariaaedrda <108534208+mariaaedrda@users.noreply.github.com>
2024-04-04 11:12:18 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-03-22T00:00:00");
-let tempoAtual = new Date( );
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const tempoObjetivo1 = new Date("2024-04-07T00:00:00");
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo (tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+}
 
 for (let i=0; i < botoes.length; i++){
     botoes[i].onclick=function(){
